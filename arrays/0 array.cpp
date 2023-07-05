a9b4aaee23f47cec43860ef60b85e916078ca1c9
Tanay <85224711+Tanaym7@users.noreply.github.com>
2023-07-05 14:59:36 +0530

    Create 0 array.cpp
    Check if Array Is Sorted and Rotated (leetcode - 1752)

---
+++ arrays/0 array.cpp
@@ -0,0 +1,34 @@
+//soution class
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count=0;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if(nums[n-1]>nums[0]){
+            count++;
+        }
+        return count<=1;
+    }
+};
+
+// driver code
+int main(){
+   int t=1;
+   //cin >> t;
+   while (t--){
+   int n;
+   cin >> n; 
+   vector<int>arr(n); 
+   for (int i = 0; i < n; i++){ 
+     cin >> arr[i];
+}
+     Solution ob; 
+     cout << ob.check(arr, n) << "\n";
+}
+   return 0;
+}
