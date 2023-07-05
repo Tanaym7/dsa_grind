9b03c5f46a51550a166bd715125d36f752dd6f54
Tanay <85224711+Tanaym7@users.noreply.github.com>
2023-07-05 20:45:34 +0530

    left rotate arrray by k steps

--- arrays/2 arrays.cpp
+++ arrays/2 arrays.cpp
@@ -1,60 +1,40 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define pb push_back
-#define fr(a,b) for(int i = a; i < b; i++)
-#define rep(i,a,b) for(int i = a; i < b; i++)
-#define mod 1000000007
-#define inf (1LL<<60)
-#define all(x) (x).begin(), (x).end()
-#define prDouble(x) cout << fixed << setprecision(10) << x
-#define triplet pair<ll,pair<ll,ll>>
-#define goog(tno) cout << "Case #" << tno <<": "
-#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define read(x) int x; cin >> x
-using namespace std;
-void init_code(){
-    fast_io;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif 
-}
-//function to return rotated array
-vector<int> rotate(vector<int> &arr, int n)
-    {
-        //code here
-        int temp=arr[0];
-    for(int i=0;i<n-1;i++){
-        arr[i]=arr[i+1];
-    }
-    arr[n-1]=temp;
-    return arr;
-	}
- 
-//printing the vector array
- void print_vector(vector<int> &arr)
-   {  
-       for (auto i = arr.begin(); i != arr.end(); ++i)
-        {cout<< *i << " ";}
-   }
- 
-int main()
-{
-	init_code();
-	int t;
-	cin >> t;
-	while (t--)
-{
-	int n;
-	cin >> n; 
-	vector<int>arr(n); 
-	for (int i = 0; i < n; i++)
-{
- cin >> arr[i];
-}
-	vector<int> even_vect=rotate(arr,n);
-   print_vector(even_vect);
 
+class Solution{		
+	public:
+	void leftRotate(int arr[], int d, int n) 
+	{ 
+	   // Your code goes here
+	   d = d % n;
+	   int temp[d];
+	   for(int i=0;i<d;i++){
+	       temp[i]=arr[i];
+	   }
+	   
+	   for(int i=d;i<n;i++){
+	       arr[i-d] = arr[i];
+	   }
+	   
+	   for(int i=n-d;i<n;i++){
+	       arr[i] = temp[i-(n-d)];
+	       
+	   }
+	   
+	} 
+		
+};
+// driver code
+int main(){
+   int t=1;
+   //cin >> t;
+   while (t--){
+   int n;
+   cin >> n; 
+   vector<int>arr(n); 
+   for (int i = 0; i < n; i++){ 
+     cin >> arr[i];
 }
-	return 0;
+    Solution ob; 
+    cout << ob.check(arr, n) << "\n";
+}
+   return 0;
 }
